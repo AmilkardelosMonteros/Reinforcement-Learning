6cd8eb1db40057b5afb272b88ff2ffcd3bb08215
AmilkardelosMonteros <48932036+AmilkardelosMonteros@users.noreply.github.com>
2019-09-23 22:12:44 -0500

    Update TD0.py

--- TD0.py
+++ TD0.py
@@ -1,70 +1,65 @@
 from agent import Agent
 from board import TicTacToe
-def recom(x):
-    if x[0]==x[1]==x[2]==1:
-        return 1
-    elif x[3]==x[4]==x[5]==1:
-        return 1
-    elif x[6]==x[7]==x[8]==1:
-        return 1
-    elif x[0]==x[3]==x[6]==1:
-        return 1
-    elif x[1]==x[4]==x[7]==1:
-        return 1
-    elif x[2]==x[5]==x[8]==1:
-        return 1
-    elif x[0]==x[4]==x[8]==1:
-        return 1
-    elif x[2]==x[4]==x[6]==1:
-        return 1
-    elif x[0]==x[1]==x[2]==2:
-        return -1
-    elif x[3]==x[4]==x[5]==2:
-        return -1
-    elif x[6]==x[7]==x[8]==2:
-        return -1
-    elif x[0]==x[3]==x[6]==2:
-        return -1
-    elif x[1]==x[4]==x[7]==2:
-        return -1
-    elif x[2]==x[5]==x[8]==2:
-        return -1
-    elif x[0]==x[4]==x[8]==2:
-        return -1
-    elif x[2]==x[4]==x[6]==2:
-        return -1
-    else:
-        return 0
+
+
+class AgentTD0(Agent):
+    '''
+        Agente que calcula Vpi con TD0
+    '''
+
+    def V(self,s):
+        default = self.values.get(s)
+        Vs = 0 if default == None else default
+        return Vs
+
+    def aux(self,x,y,z):
+        self.values[x] = self.V(x) + self.alfa*(y + self.gamma*self.V(z) - self.V(x))
+        
+    def paso(self):
+        estados = [ ]
+        rewards =  [ ]
+        self.values[self.key] = 0
+        reflected = False
+        rots = 0
+        while True:
+            is_done, new_key, reflected, rots ,reward = self.update_dicts(reflected, rots, 'X')
+            self.key = self.reset_key() if is_done else new_key
+            estados.append(new_key)
+            rewards.append(reward)
+            if is_done: 
+                reflected, rots = self.reset_rr()
+                break
+            else:
+                is_done, new_key, reflected, rots, reward= self.update_dicts(reflected, rots, 'O')
+                self.key = self.reset_key() if is_done else new_key
+                estados.append(new_key)
+                rewards.append(reward)
+                if is_done:
+                    reflected, rots = self.reset_rr()
+                    break
+        self.key = self.reset_key()
+        estados.append(0)
+        rewards.append(0)
+        return estados,rewards
 
     def num_to_matrix(self,num):
         x = self.state_to_matrix(self.base10_to_state(num))
         return x
+
     
-    def TD1(self,n):
-        self.values[self.key] = 0
-        reflected = False
-        rots = 0
+    def actualiza(self,x,y):
+        for i in range(len(x)-1):
+            St,Rt,St_prima = x[i],y[i],x[i+1]
+            self.aux(St,Rt,St_prima)
+            
+    def TD0(self,n):
         for _ in range(n):
-            is_done, new_key, reflected, rots = self.update_dicts(reflected, rots, 'X')
-            self.key = self.reset_key() if is_done else new_key
-            state = self.base10_to_state(new_key)
-            default = self.values.get(new_key)
-            Vs = 0 if default == None else default
-            print(state)
-            if is_done:
-                Rt = recom(state)
-                self.values[new_key] = Vs + self.alfa*(Rt -Vs)
-                reflected, rots = self.reset_rr()
-            else:
-                is_done, new_key, reflected, rots = self.update_dicts(reflected, rots, 'O')
-                self.key = self.reset_key() if is_done else new_key
-                default = self.values.get(new_key)
-                Rt_mas_uno = recom(self.base10_to_state(new_key)) 
-                Vs_prima = 0 if default == None else default
-                self.values[new_key] = Vs + self.alfa*(Rt_mas_uno + self.gamma*Vs_prima - Vs)
-                if is_done:
-                    reflected, rots = self.reset_rr()
-        self.key = self.reset_key()
+            x,y = self.paso()
+            self.actualiza(x,y)
+    
+        
+
+     
     
 
 
