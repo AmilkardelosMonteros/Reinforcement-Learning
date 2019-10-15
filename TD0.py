4001b50f90ffec3a858f3833f291e5ebacfa650a
AmilkardelosMonteros <48932036+AmilkardelosMonteros@users.noreply.github.com>
2019-10-14 22:29:55 -0500

    Update TD0.py

--- TD0.py
+++ TD0.py
@@ -4,25 +4,25 @@ from board import TicTacToe
 
 class AgentTD0(Agent):
     '''
-        Agente que calcula Vpi con TD0
+        Calcula Vpi con TD0
     '''
 
     def V(self,s):
         default = self.values.get(s)
         Vs = 0 if default == None else default
         return Vs
-
+    
     def aux(self,x,y,z):
         self.values[x] = self.V(x) + self.alfa*(y + self.gamma*self.V(z) - self.V(x))
         
-    def paso(self):
-        estados = [ ]
-        rewards =  [ ]
+    def episodio(self):
+        estados = []
+        rewards =  []
         self.values[self.key] = 0
         reflected = False
         rots = 0
         while True:
-            is_done, new_key, reflected, rots ,reward = self.update_dicts(reflected, rots, 'X')
+            is_done, new_key, reflected, rots ,reward = self.update(reflected, rots, 'X')
             self.key = self.reset_key() if is_done else new_key
             estados.append(new_key)
             rewards.append(reward)
@@ -30,7 +30,7 @@ class AgentTD0(Agent):
                 reflected, rots = self.reset_rr()
                 break
             else:
-                is_done, new_key, reflected, rots, reward= self.update_dicts(reflected, rots, 'O')
+                is_done, new_key, reflected, rots, reward= self.update(reflected, rots, 'O')
                 self.key = self.reset_key() if is_done else new_key
                 estados.append(new_key)
                 rewards.append(reward)
@@ -38,8 +38,6 @@ class AgentTD0(Agent):
                     reflected, rots = self.reset_rr()
                     break
         self.key = self.reset_key()
-        estados.append(0)
-        rewards.append(0)
         return estados,rewards
 
     def num_to_matrix(self,num):
@@ -49,16 +47,19 @@ class AgentTD0(Agent):
     
     def actualiza(self,x,y):
         for i in range(len(x)-1):
-            St,Rt,St_prima = x[i],y[i],x[i+1]
+            St,Rt,St_prima = x[i],y[i+1],x[i+1]
             self.aux(St,Rt,St_prima)
             
     def TD0(self,n):
         for _ in range(n):
-            x,y = self.paso()
+            x,y = self.episodio()
             self.actualiza(x,y)
     
         
 
+p = AgentTD0()
+p.TD0(100)
+print(p.values)
      
     
 
